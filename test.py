ff6e6cd600a8a3e42c35be2087e41bc1c31b2b8d
Ashutosh Singh <ashutosh.singh.de@gmail.com>
2022-08-12 05:05:44 +0200

    Add a transform

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import torch
 from torchvision.transforms import Resize
 from torch.nn import Softmax
-from torch import from_numpy, device, cuda
+from torch import from_numpy, device, cuda, set_grad_enabled
 
 import numpy as np
 from PIL import Image
@@ -29,16 +29,20 @@ def load_model(model_path):
 def predict(model, image):
     dev = device("cuda:0" if cuda.is_available() else "cpu")
 
+    model.eval()
+    with set_grad_enabled(False):
+        model = model.to(dev)
+        image = image.to(dev)
 
-    model = model.to(dev)
-    image = image.to(dev)
+        softmax = Softmax(dim=1)
 
-    softmax = Softmax(dim=1)
+        logits = model(image)
+        preds = softmax(logits)
+        top_1_class = torch.argmax(preds, dim=1)
 
-    logits = model(image)
-    preds = softmax(logits)
-    top_1_class = torch.argmax(preds, dim=1)
-    print(f'Predicted Class: {ClassificationDataset.CLASS_MAP_BY_INDEX[top_1_class[0].cpu().item()]}')
+    class_id = ClassificationDataset.CLASS_MAP_BY_INDEX[top_1_class[0].cpu().item()]
+    print(f'Predicted Class: {class_id}')
+    return class_id
 
 
 if __name__ == '__main__':
