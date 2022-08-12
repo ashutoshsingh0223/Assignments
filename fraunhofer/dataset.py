593d4eb637b06085310c509ab0948cd9f0bbe6c2
Ashutosh Singh <ashutosh.singh.de@gmail.com>
2022-08-12 16:09:58 +0200

    Add train2

--- fraunhofer/dataset.py
+++ fraunhofer/dataset.py
@@ -13,20 +13,17 @@ from fraunhofer.constants import BASE_DIR, BEACH, HARBOR, RIVER
 class ClassificationDataset(Dataset):
     CLASS_MAP_BY_INDEX = (BEACH, HARBOR, RIVER, )
 
-    def __init__(self, root: 'Path' = BASE_DIR / 'dataset', train: bool = True, download=False, transform=None):
+    def __init__(self, root: 'Path' = BASE_DIR / 'dataset', train: bool = True, download=False, transform=None,
+                 reverse=True):
         self.root = root
 
         class_0_path = self.root / self.CLASS_MAP_BY_INDEX[0]
         class_1_path = self.root / self.CLASS_MAP_BY_INDEX[1]
         class_2_path = self.root / self.CLASS_MAP_BY_INDEX[2]
 
-        class_0_files = sorted([f for f in class_0_path.glob('**/*')])
-        class_1_files = sorted([f for f in class_1_path.glob('**/*')])
-        class_2_files = sorted([f for f in class_2_path.glob('**/*')])
-
-        random.shuffle(class_0_files)
-        random.shuffle(class_1_files)
-        random.shuffle(class_2_files)
+        class_0_files = sorted([f for f in class_0_path.glob('**/*')], reverse=reverse)
+        class_1_files = sorted([f for f in class_1_path.glob('**/*')], reverse=reverse)
+        class_2_files = sorted([f for f in class_2_path.glob('**/*')], reverse=reverse)
 
         index = len(class_1_files) - len(class_1_files) // 5
         if train:
